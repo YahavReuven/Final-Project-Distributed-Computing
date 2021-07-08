6b8cb13bdf266e755ea4576c92fbe0b3e16f6266
YahavReuven <yahavre100@gmail.com>
2021-07-09 00:25:59 +0300

    added handle tasks

---
+++ Server/handle_tasks_database.py
@@ -0,0 +1,56 @@
+import os
+import base64
+from fastapi import File, UploadFile, Body
+from pydantic import BaseModel
+
+
+class ProjectInfo(BaseModel):
+    creator_id: int
+    zip_project: str
+    #zip_project: UploadFile
+
+
+class ReturnTasks(BaseModel):
+    worker_id: int
+
+# send request:
+# import base64
+# b64 = base64.b64encode(b'\x54\x43\x65')
+# resp = requests.post('http://127.0.0.1:8000/new_project', json={'creator_id': 1,'zip_project': b64.decode('utf-8')})
+
+#TODO: make it work with UploadFile insted of bytes
+async def create_new_project(new_project: ProjectInfo) -> int:
+    if not os.path.isdir('./projects'):
+        os.mkdir('./projects')
+
+    x = base64.b64decode(new_project.zip_project)
+
+    with open('./projects/file.txt.', 'wb') as file:
+        file.write(x)
+    print(new_project.creator_id, new_project.zip_project)
+    return new_project.creator_id
+
+
+# async def create_new_project(creator_id: int = Body(), somthing: int = Body()) -> int:
+#     print(creator_id)
+#     return somthing
+
+# async def create_new_project(new_project: ProjectInfo) -> int:
+#     print(new_project.creator_id)
+#     return new_project.creator_id
+
+# async def create_new_project(new_project: ProjectInfo, file: UploadFile = File(...)):
+#     #print(new_project.dict())
+#     return True
+
+
+async def return_project_results():
+    pass
+
+
+async def get_new_task():
+    pass
+
+
+async def return_task_results():
+    pass
--- Server/server.py
+++ Server/server.py
@@ -3,13 +3,18 @@ from fastapi import FastAPI
 
 from handle_devices_database import register_device, Device, get_device_database, delete_devices_database, init_devices_database, delete_devices_database
 
+from handle_tasks_database import create_new_project
+
 app = FastAPI()
 
-init_devices_database()
+#init_devices_database()
 
 register_device = app.post("/register_device", response_model=Device)(register_device)
 get_device_database = app.get('/devices')(get_device_database)
 delete_devices_database = app.get('/delete')(delete_devices_database)
+
+
+create_new_project = app.post('/new_project')(create_new_project)
 
 
 if __name__ == "__main__":
