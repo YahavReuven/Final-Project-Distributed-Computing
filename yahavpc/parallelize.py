740d0523ff47d37a04bd6c93ae9641d2540bf673
YahavReuven <yahavre100@gmail.com>
2021-08-12 00:07:42 +0300

    distribute changed to class decorator factory

--- yahavpc/parallelize.py
+++ yahavpc/parallelize.py
@@ -1,5 +1,5 @@
 
-import asyncio
+import time
 from collections.abc import Iterable
 
 import dill
@@ -7,6 +7,55 @@ import dill
 
 import requests
 import base64
+
+
+
+class Distribute:
+    def __init__(self, iterable: Iterable, task_size: int):
+        self.iterable = iterable
+        self.task_size = task_size
+
+    class Decorator:
+
+        # def __init__(self):
+        #     # self.cls = cls
+
+        def __call__(self):
+            # device = requests.get('http://127.0.0.1:8000/register_device')
+            # print(device.text[1:-1])
+            # serialized_class = dill.dumps(cls)
+            # serialized_iterable = dill.dumps(self.iterable)
+            #
+            # encoded_class = base64.b64encode(serialized_class).decode('utf-8')
+            # encoded_iterable = base64.b64encode(serialized_iterable).decode('utf-8')
+            # project1 = requests.post('http://127.0.0.1:8000/upload_new_project',
+            #                          json={'creator_id': device.text[1:-1],
+            #                                'base64_serialized_class': encoded_class,
+            #                                'base64_serialized_iterable': encoded_iterable})
+            # print(project1.text[1:-1])
+
+            # TODO: deal with imports
+            print(1)
+            return self.get_results
+
+        def get_results(self):
+            results = None
+            i = 0
+            while not results:
+                if i == 10:
+                    results = 1
+                    break
+                print('asking server')
+
+                time.sleep(5)
+                i += 1
+            return results
+
+    def __call__(self, cls):
+        print('in __call__ decorator factory')
+        return self.Decorator()
+
+
 
 def distribute(iterable: Iterable, task_size: int):
 
@@ -27,7 +76,7 @@ def distribute(iterable: Iterable, task_size: int):
                     break
                 print('asking server')
 
-                asyncio.sleep(5)
+                time.sleep(1)
                 i += 1
             return results
 
@@ -56,7 +105,7 @@ def distribute(iterable: Iterable, task_size: int):
 
 
 
-@distribute(range(100), 10)
+@Distribute(range(100), 10)
 class A:
     @staticmethod
     def a():
@@ -66,4 +115,7 @@ class A:
         return 'b'
 
 
-x = A()
+# A = Distribute(range(100), 10)(A)
+get_results = A()
+get_results()
+
